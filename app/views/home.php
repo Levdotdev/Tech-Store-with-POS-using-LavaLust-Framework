4f517a6612cb270314033ae5f4ce6693d3bc9f2e
Levdotdev <britolancekianne@gmail.com>
2025-09-29 06:36:43 +0800

    icon & title

--- app/views/home.php
+++ app/views/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Genshin CRUD-pact</title>
+    <title>Home - GENSHIN CRUD</title>
     <link rel="icon" type="image/x-icon" href="<?= base_url();?>public/resources/logo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
