4bdfe6706fce380a671725fdadc581490ee4b6e7
Levdotdev <britolancekianne@gmail.com>
2025-11-13 10:59:43 +0800

    redirect

--- app/config/routes.php
+++ app/config/routes.php
@@ -42,10 +42,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 |
 */
-
-$router->group('', function() use ($router){
-    $router->get('', 'Auth::login');
-});
+$router->get('', 'Admin/ProductController::index');
 
 $router->group('/product', function() use ($router){
     $router->get('/home', 'Admin/ProductController::index');
