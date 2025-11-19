3221c315ff6a888b4e190d979a5d7372806a7928
Levdotdev <britolancekianne@gmail.com>
2025-11-20 04:32:48 +0800

    add

--- app/controllers/CrudController.php
+++ app/controllers/CrudController.php
@@ -176,11 +176,13 @@ class CrudController extends Controller {
                 'price' => $price
                 ];
 
-                if ($this->ProductModel->insert($data)) {
+                $this->ProductModel->insert($data);
+
+                /*if ($this->ProductModel->insert($data)) {
                     $this->session->set_flashdata('message', 'Product inserted successfully!');
                 } else {
                     $this->session->set_flashdata('error', 'Something went wrong.');
-                }
+                }*/
 
                 redirect();
         }
