ff98c55329674fc704dc8c0d385966cae7f01e9c
Levdotdev <britolancekianne@gmail.com>
2025-11-20 09:42:44 +0800

    default value

--- app/views/modals/inventory_Update.php
+++ app/views/modals/inventory_Update.php
@@ -4,7 +4,7 @@
             <div class="modal-body">
                 <form id="form-record-stock" method="post" action="<?=site_url('update-stock'); ?>" autocomplete="off">
                     <div class="form-group"><label for="product_id">Product</label><div class="input-group"><i class="fas fa-box"></i><input type="text" id="product_id" name="product_id" placeholder="Scan unique product code" required></div></div>
-                    <div class="form-group"><label for="stock">Quantity Added</label><div class="input-group"><input type="number" id="stock" name="stock" placeholder="1" step="1" min="1" required></div></div>
+                    <div class="form-group"><label for="stock">Quantity Added</label><div class="input-group"><input type="number" id="stock" name="stock" placeholder="1" step="1" min="1" value="1" required></div></div>
                 </form>
             </div>
             <div class="modal-footer">
