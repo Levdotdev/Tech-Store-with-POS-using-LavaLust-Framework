82272feadd4ba7474a1b5bf8a9bec66f7fa2587e
Levdotdev <britolancekianne@gmail.com>
2025-11-21 11:53:06 +0800

    dump

--- app/controllers/_AdminController.php
+++ app/controllers/_AdminController.php
@@ -87,7 +87,7 @@ class _AdminController extends Controller {
 
         $data['sales'] = $this->db->table('transactions')->select_sum('total', 'total')->get();
         $data['sold'] = $this->db->table('products')->select_sum('sold', 'sold')->get();
-        $res = $this->db->raw('SELECT COUNT(stock) FROM products', [1]);
+        $res = $this->db->raw('SELECT COUNT(stock) FROM products');
         $data['low_stock'] = $res->fetch();
 
         $this->call->view('home', $data);
